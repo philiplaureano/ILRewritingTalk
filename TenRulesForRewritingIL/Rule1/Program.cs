f2e9c77499fb5fcc46227507bfaa4dd2726ead73
Philip Laureano <philip.laureano@gmail.com>
2011-04-25 15:22:09 +0800

    Added Rule #1 code samples

---
+++ TenRulesForRewritingIL/Rule1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rule1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 42;
+            if (number % 2 == 0)
+                Console.WriteLine("Hello, World!");
+            else
+                Console.WriteLine("Hello, NDC!");
+        }
+    }
+}
