075b4cc6516f1c4ffa6c46d2939afb15256301db
Philip Laureano <philip.laureano@gmail.com>
2011-04-25 19:39:22 +0800

    Added sample code for Rule #2

---
+++ TenRulesForRewritingIL/Rule2/Rule2.Interfaces/IGreeter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rule2.Interfaces
+{
+    public interface IGreeter
+    {
+        void Greet(string target);
+    }
+}
---
+++ TenRulesForRewritingIL/Rule2/Rule2.Library/ConsoleGreeter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Rule2.Interfaces;
+
+namespace Rule2.Library
+{
+    public class ConsoleGreeter : IGreeter
+    {
+        public void Greet(string target)
+        {
+            Console.WriteLine("Hello, {0}!", target);
+        }
+    }
+}
